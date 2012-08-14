bb44cd8730d1afa050a246780045f1313970b436
LStuker <lucien.stuker@gmail.com>
2012-08-14 23:36:52 +0200

    #20: Create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Menantang::Application.config.secret_token = 'c137c5abe3e308c661d10e7cbe7ffcebd99f9e1a764cf77e5702afa11636603c501b9a67162b6b87ec012ea7cba752d210dfe58adc0fae7ecd8761510d2a04e0'
