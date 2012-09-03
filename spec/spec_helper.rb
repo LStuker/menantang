21be3713a75e6c0089227a2c47ccb8008bd8fd29
LStuker <lucien.stuker@gmail.com>
2012-09-03 22:55:26 +0200

    change spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,11 +8,10 @@ Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However, 
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
+
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'views/inherited_resource_helpers'
-
-  # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 end
@@ -26,11 +25,17 @@ Spork.each_run do
   load 'spec/factories.rb'
 end
 
-# == Mock Framework
-RSpec.configuration.mock_with :rspec
+RSpec.configure do |config|
 
-# If you're not using ActiveRecord, or you'd prefer not to run each of your
-# examples within a transaction, remove the following line or assign false
-# instead of true.
-RSpec.configuration.use_transactional_fixtures = true
-RSpec.configuration.use_transactional_examples = true
+  # == Mock Framework
+  config.mock_with :rspec
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
+  config.use_transactional_examples = true
+
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+end
