f0666c60445cc4d4849b2fcd5645f1c5ef326a38
LStuker <lucien.stuker@gmail.com>
2012-11-11 18:10:55 +0100

    change routes to support direct links

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Menantang::Application.routes.draw do
 
-  get "static_pages/home"
+  root to: "static_pages#home"
 
-  get "static_pages/help"
+  match '/help' ,to: "static_pages#help"
 
-  get "static_pages/about"
+  match '/about', to: "static_pages#about"
+
+  match '/terms', to: "static_pages#terms"
+
+  match '/privacy', to: "static_pages#privacy"
 
   devise_for :users
 
